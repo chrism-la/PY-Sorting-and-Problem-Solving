7a11f6ba25b5d3a292c1ecd0ea215af0134f08f8
Chris <117781333+chrism-la@users.noreply.github.com>
2024-03-25 19:51:54 -0700

    used sort method and sorted function

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
 # Write your solution for algorithm 1 below
+def sort_list(list):
+  list.sort()
+  
+  
+
+unsorted_list = [5,4,2,3,1]
+sort_list(unsorted_list)
+print(unsorted_list)
--- 2.py
+++ 2.py
@@ -1,1 +1,10 @@
 # Write your solution for algorithm 2 below
+def sort_list(list):
+  sorted_list = sorted(list)
+  return sorted_list
+
+
+arr = [5,3,4,2,1]
+
+print(sort_list(arr))
+print(arr)
